d7c398ccfbc343aa0efbbcb1bb8469ac3412918d
Leonardo Torres Briones <dleonardotb2001@gmail.com>
2020-08-30 18:19:25 -0500

    Añadido login (maqueta)

--- Proyecto/controllers/UsuarioController.php
+++ Proyecto/controllers/UsuarioController.php
@@ -3,5 +3,13 @@
         public function index(){
             echo "Controlador usuario, Accion index";
         }
+        public function registro(){
+            require_once 'views/usuario/registro.php';
+        }
+        public function save(){
+            if(isset($_POST)){
+                var_dump($_POST);
+            }
+        }
     }
 ?>
---
+++ Proyecto/views/usuario/registro.php
@@ -0,0 +1,16 @@
+<div id="registro">
+    <h1>Registrese en Paruro E-commerce</h1>
+    <form action="index.php?controller=usuario&action=save" method="POST">
+        <label for="nombre">Nombre</label>
+        <input type="text" name="nombre" placeholder="Ingrese su nombre" required/>
+        <label for="apellidos">Apellidos</label>
+        <input type="text" name="apellidos" placeholder="Ingreses su apellidos" required/>
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Ingrese su email" required/>
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" placeholder="Ingrese su contraseña" required/>
+        <input type="submit" value="Registrar"/>
+    </form>
+</div>     
+
+ 
